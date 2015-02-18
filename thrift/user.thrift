2fb16829cc7295e7a9b78a7d44f57fd8b114a21a
Alex Milstead <alex@amilstead.com>
2015-02-17 20:32:16 -0800

    Change user struct name.

--- thrift/user.thrift
+++ thrift/user.thrift
@@ -3,7 +3,7 @@ include "core.thrift"
 namespace csharp Services.User
 namespace py _thrift.services.user
 
-struct User {
+struct UserModel {
   1: i32 user_id
   2: string username
   3: string password
@@ -13,9 +13,9 @@ service User extends core.SharedService {
 
    bool remove(1:i32 user_id),
 
-   User create(1:string username, 2:string password),
+   UserModel create(1:string username, 2:string password),
 
-   User get(1:string username),
+   UserModel get(1:string username),
 
    bool login(1:string username, 2:string password),
 
