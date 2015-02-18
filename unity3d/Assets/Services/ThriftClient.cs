f16a929a629b703a22db0ef8b7b8deca573c1bdf
Alex Milstead <alex@amilstead.com>
2015-02-17 20:47:05 -0800

    Add example Unity3D project + sources.

---
+++ unity3d/Assets/Services/ThriftClient.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Thrift;
+using Thrift.Protocol;
+using Thrift.Transport;
+using Services;
+
+
+public class ThriftClient : IDisposable
+{
+    private string serviceName;
+    private string serviceHost;
+    private int servicePort;
+    private TTransport transport;
+    private TProtocol baseProtocol;
+    private TMultiplexedProtocol protocol;
+
+    private Dictionary<string, string> serviceMap = new Dictionary<string, string>() {
+        {"User.v1", "Services.User.User+Client"}
+    };
+
+    public ThriftClient(string serviceName, string serviceHost = "127.0.0.1", int servicePort = 8001)
+    {
+        this.serviceName = serviceName;
+        this.serviceHost = serviceHost;
+        this.servicePort = servicePort;
+        this.InitProtocol();
+    }
+
+    private void InitProtocol()
+    {
+        this.transport = new TFramedTransport(new TSocket(this.serviceHost, this.servicePort));
+        this.baseProtocol = new TBinaryProtocol(this.transport);
+        this.protocol = new TMultiplexedProtocol(this.baseProtocol, this.ServiceName);
+    }
+
+    public SharedService.Iface GetClient()
+    {
+        string className = this.serviceMap[this.serviceName];
+        Type objectType = Type.GetType(className);
+        SharedService.Iface client = (SharedService.Iface)Activator.CreateInstance(objectType, this.protocol);
+        this.transport.Open();
+        return client;
+    }
+
+    public string ServiceName
+    {
+        get
+        {
+            return this.serviceName;
+        }
+    }
+
+    public void Dispose()
+    {
+        this.transport.Close();
+    }
+}
---
+++ unity3d/Assets/Startup.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using Services;
+using Services.User;
+
+
+public class Startup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        using (ThriftClient clientService = new ThriftClient("User.v1"))
+        {
+            User.Client client = (User.Client)clientService.GetClient();
+            UserModel newUser = client.create("foo", "bar");
+            string response = "Creation failed!";
+            if (newUser.User_id != 0)
+            {
+                response = "Successfully created user!";
+            }
+            Debug.Log(response);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
