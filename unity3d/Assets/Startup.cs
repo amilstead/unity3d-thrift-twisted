fcf81c27fab4381aea9c20c8875f3be48fcc5383
Alex Milstead <alex@amilstead.com>
2015-02-17 22:03:15 -0800

    Add Unity3D code to use thrift

--- unity3d/Assets/Startup.cs
+++ unity3d/Assets/Startup.cs
@@ -8,16 +8,65 @@ public class Startup : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        using (ThriftClient clientService = new ThriftClient("User.v1"))
+        using (ThriftClient clientService = new ThriftClient("User.v1", "192.168.1.114"))
         {
             User.Client client = (User.Client)clientService.GetClient();
-            UserModel newUser = client.create("foo", "bar");
-            string response = "Creation failed!";
-            if (newUser.User_id != 0)
+            UserModel user = null;
+            string username = "foo";
+            string password = "bar";
+            bool success = client.login(username, password);
+            if (success)
             {
-                response = "Successfully created user!";
+                Debug.Log("Login Success!");
             }
-            Debug.Log(response);
+            else
+            {
+                Debug.Log("Login failed!");
+                Debug.Log("Trying to create user");
+                // try to create and login
+                user = client.create(username, password);
+                if (user.User_id != 0) {
+                    Debug.Log("Created user!");
+                    Debug.Log(user.ToString());
+                    Debug.Log("Trying a login now.");
+                    success = client.login(user.Username, user.Password);
+                    if (success)
+                    {
+                        Debug.Log("Login succeeded!");
+                    }
+                    else
+                    {
+                        Debug.Log("login failed!");
+                    }
+                }
+                else
+                {
+                    Debug.Log("User creation failed!");
+                }                
+            }
+
+            user = client.get(username);
+            if (user.User_id != 0)
+            {
+                Debug.Log("Got user from client:");
+                Debug.Log(user.ToString());
+
+                bool removed = client.remove(user.User_id);
+                if (removed)
+                {
+                    Debug.Log("Successfully removed user with id: " + user.User_id);
+                }
+                else
+                {
+                    Debug.Log("Failed to remove user!");
+                }
+            }
+            else
+            {
+                Debug.Log("Failed to get user with username: " + username);
+            }
+
+            
         }
 	}
 	
